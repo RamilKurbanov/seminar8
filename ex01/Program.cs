fb2152dea9f9ba78d42ee12a7424074b9f99e28a
Ramil <ramilkurbanov666@gmail.com>
2022-11-24 17:28:16 +0300

    задачи от 8 семинара

---
+++ ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿double[,] GetArray(int row, int column, double minValue, double maxVlaue)
+{
+    double[,] result = new double[row, column];
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            result[i,j] = Math.Round(new Random().NextDouble() * (maxVlaue - minValue) + minValue, 2);
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(double[,] array)
+{
+    Console.WriteLine("Двумерный масси: ");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            Console.WriteLine($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Задайте количество строк m = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Задайте количество строк n = ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите нижнюю границу диапазона элементов массива:  ");
+double lower = double.Parse(Console.ReadLine()!);
+Console.Write("Введите верхнюю границу диапазона элементов массива:  ");
+double upper = double.Parse(Console.ReadLine()!);
+
+double[,] workingArray = GetArray(m, n, lower, upper);
+PrintArray(workingArray);
